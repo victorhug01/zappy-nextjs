5bb07a02a1ddaaf2c262a8f99d3b46544432b83e
victor <victorhugobbpereira02@gmail.com>
2025-07-27 22:24:28 -0300

    adicionando animação com gsap e removendo fonte lato

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,14 +1,37 @@
 'use client'
 
-import Image from "next/image";
+import gsap from 'gsap';
 import Link from "next/link";
+import Image from "next/image";
+import { useLayoutEffect } from 'react'
 import DrawerComponent from "./components/drawer";
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export default function HomeClient() {
+
+    useLayoutEffect(() => {
+
+        gsap.registerPlugin(ScrollTrigger)
+
+        gsap.to(".macbook",{
+            width: "100%",
+            scrollTrigger: {
+                trigger: '.mackbookAnimation',
+                // markers: true,
+                end: "top 80%",
+                scrub: true,
+            }
+        })
+
+        return () => {
+            gsap.killTweensOf('.mackbookAnimation');
+        }
+    }, [])
+
     return (
         <div>
             <header>
-                <nav className="w-full h-16 flex bg-background fixed justify-between items-center px-3 overflow-x-hidden" aria-label="Navbar Desktop">
+                <nav className="w-full h-16 flex bg-background fixed justify-between items-center px-3 overflow-x-hidden z-50" aria-label="Navbar Desktop">
                     <Link href={'/'}><Image src="/zappy.webp" width={64}  className="min-w-16" height={64} alt="Brand zappy top left" priority={true} fetchPriority="high"/></Link>
                     <div className="hidden md:flex gap-8 w-full items-center lg:visible pl-4">
                         <Link href={"#home"} className="text-foreground-default hover:text-primary inline-block relative after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 after:bg-primary after:transition-transform after:duration-200 after:ease-out hover:after:scale-x-100 after:origin-bottom-left">Início</Link>
@@ -36,15 +59,20 @@ export default function HomeClient() {
                         </div>  
                     </div>
                 </section>
-                <section className="w-full h-screen flex justify-center items-center bg-primary" id="section2">
-                <h2>Sessao 2</h2>
+
+                <section className="w-full h-fit md:h-screen flex justify-center items-start mackbookAnimation" id="section2">
+                    <Image src="/mackbook.svg" width={0} height={0} className="md:max-w-5/6 transform translate-y-[-150px] lg:translate-y-[-50px] macbook" alt="macbook image" priority={true} fetchPriority="high"/>
                 </section>
-                <section className="w-full h-screen flex justify-center items-center" id="section3">
+                
+
+                <section className="w-full h-screen flex justify-center items-center bg-primary" id="section3">
                 <h2>Sessao 3</h2>
                 </section>
-                <section className="w-full h-screen flex justify-center items-center bg-primary" id="section4">
+
+                <section className="w-full h-screen flex justify-center items-center" id="section4">
                 <h2>Sessao 4</h2>
                 </section>
+
             </main>
 
             <footer className="w-full h-20 bg-blue-800 text-white flex justify-center items-center">
